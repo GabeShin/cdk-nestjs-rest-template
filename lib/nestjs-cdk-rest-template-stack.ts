16e1537ebd6bf77bf107ed601354eba75e22180d
Gabe <gabeshin.ts@gmail.com>
2022-05-12 23:27:01 +0900

    CDK using nestjs REST api template implementation

--- lib/nestjs-cdk-rest-template-stack.ts
+++ lib/nestjs-cdk-rest-template-stack.ts
@@ -1,16 +1,42 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+	AssetCode,
+	Function,
+	LayerVersion,
+	Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NestjsCdkRestTemplateStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		const dependenciesLayer = new LayerVersion(this, "DependenciesLayer", {
+			code: new AssetCode("modules/test_api/node_modules"),
+			compatibleRuntimes: [Runtime.NODEJS_14_X, Runtime.NODEJS_16_X],
+		});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NestjsCdkRestTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const testLambda = new Function(this, "TestHandler", {
+			runtime: Runtime.NODEJS_16_X,
+			code: new AssetCode("modules/test_api/dist"),
+			handler: "index.handler",
+			layers: [dependenciesLayer],
+			environment: {
+				NODE_PATH: "$NODE_PATH:/opt",
+			},
+		});
+
+		new LambdaRestApi(this, "TestEndpoint", {
+			handler: testLambda,
+		});
+
+		// The code that defines your stack goes here
+
+		// example resource
+		// const queue = new sqs.Queue(this, 'NestjsCdkRestTemplateQueue', {
+		//   visibilityTimeout: cdk.Duration.seconds(300)
+		// });
+	}
 }
