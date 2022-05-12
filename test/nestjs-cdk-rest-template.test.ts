140e527d33d25573948767437ecd5b997206d22f
Gabe <gabeshin.ts@gmail.com>
2022-05-12 22:22:34 +0900

    Initial commit

---
+++ test/nestjs-cdk-rest-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestjsCdkRestTemplate from '../lib/nestjs-cdk-rest-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nestjs-cdk-rest-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestjsCdkRestTemplate.NestjsCdkRestTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
